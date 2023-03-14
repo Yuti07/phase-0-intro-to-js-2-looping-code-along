2b2d1d51197c7c357a883d5b18a1478dd13c3c86
Yuti Thakkar <thakkaryuti@gmail.com>
2023-03-13 21:33:31 -0400

    adds looping lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+
+function writeCards(names, occasion) {
+    const messages = [];
+    for (let i = 0; i < names.length; i++) {
+      messages.push(`Thank you, ${names[i]}, for the wonderful ${occasion} gift!`);
+    }
+    return messages;
+  }
+writeCards(["Guadalupe", "Ollie", "Aki"], "surprise");
+function countDown(num) {
+    while (num >= 0) {
+      console.log(num);
+      num--;
+    }
+  }
+  
